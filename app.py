92745cc51906f7fdde2c3ab6fe247c53c02a9b41
Galih Restu Baihaqi <84848229+gafisk@users.noreply.github.com>
2024-01-15 23:35:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,152 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+from assets import read_data, preprocess_and_print, pelabelan, tf_idf, proses_pca, fold, eval_akhir, fold_no
+
+if 'df' not in st.session_state:
+    st.session_state.df = None
+if 'data' not in st.session_state:
+    st.session_state.data = None
+if 'data_tf' not in st.session_state:
+    st.session_state.data_tf = None
+if 'h_pca' not in st.session_state:
+    st.session_state.h_pca = None
+
+st.set_page_config(
+    page_title="Kereta Madura",
+    page_icon=":chart_with_upwards_trend:",  # Ikoni bisa disesuaikan
+    layout="centered",  # Pilihan layout ("wide" atau "centered")
+    initial_sidebar_state="expanded",  # Kondisi awal sidebar ("expanded" atau "collapsed")
+)
+
+with st.sidebar:
+    selected = option_menu( 
+        menu_title="Main Menu",
+        options=["Home", "Dataset", "Preprocessing", "TF-IDF", "PCA", "Evaluasi"],
+        icons=["house", "book", "radioactive", "bezier2", "bounding-box", "ubuntu"],
+        menu_icon="menu-up",
+        default_index=0,
+    )
+
+if selected == "Home":
+    st.title("Home")
+    st.subheader("Model Naive Bayes dan PCA untuk Data Sentimen Masyarakat (Reaktivasi Kereta Api Madura)")
+
+if selected == "Dataset":
+    st.title("Dataset")
+    sub_dataset = option_menu(
+        menu_title="Pilih Dataset",
+        options=["Main Data", "Upload Data"],
+        icons=["database", "cloud-arrow-up"],
+        default_index=0,
+    )
+
+    if sub_dataset == "Main Data":
+        st.subheader("Main Data")
+        st.session_state.df = pd.read_excel("Dataset.xlsx")
+        st.write(st.session_state.df)
+
+    if sub_dataset == "Upload Data":
+        st.subheader("Unggah Dataset (Excel)")
+        upload_file = st.file_uploader("Pilih file Excel", type=["xlsx"])
+
+        if upload_file is not None:
+            st.success("File berhasil diunggah.")
+            st.subheader("Preview Data:")
+            st.session_state.df = read_data(upload_file)
+            if st.session_state.df is not None:
+                st.write(st.session_state.df)
+        else:
+            st.warning("Silakan unggah file excel untuk melihat data.")
+
+if selected == "Preprocessing":
+    st.title("Preprocessing")
+    sub_preprocessing = option_menu(
+        menu_title="Pilih Proeses Preprocessing",
+        options=["Main Data Preprocessing", "Manual Preprocessing"],
+        icons=["database", "repeat"],
+        default_index=0,
+    )
+
+    if sub_preprocessing == "Main Data Preprocessing":
+        st.subheader("Hasil Preprocessing")
+        st.session_state.data = pd.read_excel("Preprocessing.xlsx")
+        st.write(st.session_state.data)
+
+    if sub_preprocessing == "Manual Preprocessing":
+        if st.button("Proses Data"):
+            data = pelabelan(st.session_state.df)
+            data['text'] = data['text'].apply(preprocess_and_print)
+            st.write(data)
+            st.session_state.data = data
+            st.success("Proses Data Selesai")
+
+if selected == "TF-IDF":
+    st.title("Proses TF-IDF")
+    st.subheader("Data sebelum TF-IDF")
+    st.write(st.session_state.data)
+    if st.button("Mulai Proses TF-IDF"):
+        data = tf_idf(st.session_state.data)
+        st.subheader("Hasil TF-IDF")
+        st.write(data[0])
+        st.session_state.data_tf = data[1]
+        st.success("Proses Data Selesai")
+
+if selected == "PCA":
+    st.title("Proses PCA")
+    n = st.slider("Nilai Component PCA", min_value=1, max_value=10, value=4)
+    pca = proses_pca(st.session_state.data_tf, st.session_state.data, n)
+    if n < 6:
+        col1, col2 = st.columns(2)
+        with col1:
+            st.header('Hasil PCA')
+            st.write(pca[0])
+
+        with col2:
+            st.header('Nilai Eigen')
+            st.write(pca[1])
+    else:
+        st.header('Hasil PCA')
+        st.write(pca[0])
+        st.header('Nilai Eigen')
+        st.write(pca[1])
+    st.session_state.h_pca = pca[0]
+
+if selected == "Evaluasi":
+    st.title("Nilai Evaluasi")
+    n = st.slider("Nilai K", min_value=2, max_value=10, value=4)
+    st.subheader("Rata - Rata Evaluasi PCA")
+    evaluasi = fold(st.session_state.data_tf, st.session_state.data, n, st.session_state.h_pca)
+    rata_rata = eval_akhir(evaluasi)
+    pca1, pca2, pca3, pca4 = st.columns(4)
+    with pca1:
+        st.write("Akurasi:", round(rata_rata['Akurasi'],2))
+    with pca2:
+        st.write("Presisi:", round(rata_rata['Presisi'],2))
+    with pca3:
+        st.write("Recall:", round(rata_rata['Recall'],2))
+    with pca4:
+        st.write("F1-Score:", round(rata_rata['F1-Score'],2))
+    
+    eval_no = fold_no(st.session_state.data_tf, st.session_state.data, n)
+    rata = eval_akhir(eval_no)
+    st.subheader("Rata - Rata Evaluasi Tanpa PCA")
+    pc1, pc2, pc3, pc4 = st.columns(4)
+    with pc1:
+        st.write("Akurasi:", round(rata['Akurasi'],2))
+    with pc2:
+        st.write("Presisi:", round(rata['Presisi'],2))
+    with pc3:
+        st.write("Recall:", round(rata['Recall'],2))
+    with pc4:
+        st.write("F1-Score:", round(rata['F1-Score'],2))
+
+    eval1, eval2 = st.columns(2)
+    with eval1:
+        st.subheader(f"Nilai K-Fold PCA")
+        st.write(evaluasi)
+    with eval2:
+        st.subheader(f"Nilai K-Fold Tanpa PCA")
+        st.write(evaluasi)
+
+        
---
+++ assets.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+import re, string
+import nltk
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+nltk.download('punkt')
+nltk.download('stopwords')
+
+factory = StopWordRemoverFactory()
+stopword = factory.create_stop_word_remover()
+stemmer = StemmerFactory().create_stemmer()
+
+def read_data(file):
+    if file is not None:
+        df = pd.read_excel(file)
+        return df
+    return None
+
+
+def pelabelan(data):
+    data['label'] = data['label'].map({'positif': 1, 'negatif': 0})
+    data = data[['text', 'label']]
+    return data
+
+
+def cleaningText(text):
+    text = re.sub(r'@[A-Za-z0-9]+', '', text)
+    text = re.sub(r'#[A-Za-z0-9]+', '', text)
+    text = re.sub(r'RT[\s]', '', text)
+    text = re.sub(r"http\S+", '', text)
+    text = re.sub(r'[0-9]+', '', text)
+
+    text = text.replace('\n', ' ')
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = text.strip(' ')
+    return text
+
+def casefoldingText(text):
+    text = text.lower()
+    return text
+
+slangword_dict = {
+    'bgt': 'banget',
+    'gak': 'tidak',
+    'yg': 'yang',
+}
+
+def convert_slangwords(text, slangword_dict):
+    words = text.split()
+    converted_words = []
+    for word in words:
+        if word in slangword_dict:
+            converted_words.append(slangword_dict[word])
+        else:
+            converted_words.append(word)
+    return ' '.join(converted_words)
+
+def tokenizingText(text):
+    text = word_tokenize(text)
+    return text
+
+def filteringText(text):
+    stopwords_indonesian = stopwords.words('indonesian')
+    stopwords_indonesian.remove('ada')
+    stopwords_indonesian.remove('kurang')
+    stopwords_indonesian.remove('tidak')
+    filtered = []
+    for txt in text:
+        if txt not in stopwords_indonesian:
+            filtered.append(txt)
+    text = filtered
+    return text
+
+def stemmingText(text):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    text = [stemmer.stem(word) for word in text]
+    return text
+
+def join_text_list(data):
+    data['text'] = data['text'].apply(lambda x: ' '.join(x))
+    return data
+
+def preprocess(text):
+    text = cleaningText(text)
+    text = casefoldingText(text)
+    text = convert_slangwords(text, slangword_dict)
+    text = tokenizingText(text)
+    text = filteringText(text)
+    text = stemmingText(text)
+    return text
+
+def preprocess_and_print(text):
+    processed_text = preprocess(text)
+    print(f"Original Text: {text}")
+    print(f"Processed Text: {processed_text}")
+    print("")
+    return processed_text
+
+def tf_idf(data):
+    vectorizer = TfidfVectorizer()
+    tfidf_df = pd.DataFrame(vectorizer.fit_transform(data['text']).toarray(), columns=vectorizer.get_feature_names_out())
+    tfidf_vectors = vectorizer.fit_transform(data['text'])
+    return tfidf_df, tfidf_vectors
+
+def split_data(a, b):
+    from sklearn.model_selection import train_test_split
+    X = a
+    y = b['label']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def proses_pca(a, b, n):
+    data = split_data(a,b)
+    pca = PCA(n_components=n)
+    X_train_pca = pca.fit_transform(data[0].toarray())
+    return X_train_pca, pca.explained_variance_
+
+def fold(a, b, n, c):
+    data = split_data(a, b)[2]
+    classifier = GaussianNB()
+    kf = KFold(n_splits=n, shuffle=True, random_state=42)
+    results = []
+    for fold, (train_index, test_index) in enumerate(kf.split(c), 1):
+        X_train_fold, X_test_fold = c[train_index], c[test_index]
+        y_train_fold, y_test_fold = data.iloc[train_index], data.iloc[test_index]
+        classifier.fit(X_train_fold, y_train_fold)
+        y_pred_fold = classifier.predict(X_test_fold)
+        accuracy = round(accuracy_score(y_test_fold, y_pred_fold) * 100, 2)
+        precision = round(precision_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        recall = round(recall_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        f1 = round(f1_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        results.append({'K-Fold': fold, 'Akurasi': accuracy, 'Presisi': precision, 'Recall': recall, 'F1-Score': f1})
+    results_df = pd.DataFrame(results)
+    return results_df
+
+def fold_no(a, b, n):
+    data = split_data(a, b)
+    classifier = GaussianNB()
+    kf = KFold(n_splits=n, shuffle=True, random_state=42)
+    results_gs = []
+    for fold, (train_index, test_index) in enumerate(kf.split(data[0]), 1):
+        X_train_fold, X_test_fold = data[0][train_index], data[0][test_index]
+        y_train_fold, y_test_fold = data[2].iloc[train_index], data[2].iloc[test_index]
+        classifier.fit(X_train_fold.toarray(), y_train_fold)
+        y_pred_fold = classifier.predict(X_test_fold.toarray())
+        accuracy = round(accuracy_score(y_test_fold, y_pred_fold) * 100, 2)
+        precision = round(precision_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        recall = round(recall_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        f1 = round(f1_score(y_test_fold, y_pred_fold, average='weighted', zero_division=1) * 100, 2)
+        results_gs.append({'K-Fold': fold, 'Akurasi': accuracy, 'Presisi': precision, 'Recall': recall, 'F1-Score': f1})
+    results_gss = pd.DataFrame(results_gs)
+    return results_gss
+
+def eval_akhir(data):
+    rata_rata = data[['Akurasi', 'Presisi', 'Recall', 'F1-Score']].mean()
+    return rata_rata
